132b8b88bd8c6c2b38f3057dc8067990eb514f08
mohamed82008 <mohamed82008@gmail.com>
2019-10-08 13:30:12 +1100

    drop Julia v0.7 support

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,4 @@
-using LinearAlgebra, SparseArrays, Preconditioners, IterativeSolvers, Random
-
-@static if VERSION < v"0.7.0-DEV.2005"
-    using Base.Test
-else
-    using Test
-end
+using LinearAlgebra, SparseArrays, Preconditioners, IterativeSolvers, Random, Test
 
 Random.seed!(1)
 
