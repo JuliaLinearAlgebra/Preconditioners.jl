67a8b1bc0fcce824baeb45e60974ef03d0c7dc58
Fredrik Ekre <ekrefredrik@gmail.com>
2022-09-09 22:44:31 +0200

    Misc updates (#25)
    * DiagonalPreconditioner: Make sure diagonal is dense
    
    Since diag(::SparseMatrixCSC) return a SparseVector it is better to
    convert to a dense vector with faster indexing. The diagonal should
    always be dense for matrices where a diagonal preconditioner is used
    anyway, so this does not affect the memory usage.
    
    * DiagonalPreconditioner: fixes for offset arrays
    
    This makes sure at-inbounds is only used with valid indices.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 using LinearAlgebra, SparseArrays, Preconditioners, IterativeSolvers, Random, Test
+using OffsetArrays
 
 Random.seed!(1)
 
@@ -15,6 +16,9 @@ function test_matrix(A, F, atol)
     else
         p = F(A)
     end
+    if F === DiagonalPreconditioner
+        @test p.D isa Vector
+    end
     @test isapprox(p \ b, ldiv!(p, b), atol=atol)
     @test isapprox(ldiv!(copy(b), p, b), ldiv!(p, b), atol=atol)
     @test isapprox(cg(A, A*b, Pl=p), b, atol=atol)
@@ -26,6 +30,24 @@ function test_matrix(A, F, atol)
     A = A + A' + 30I
     UpdatePreconditioner!(p, A)
     @test isapprox(cg(A, A*b, Pl=p), b, atol=atol)
+
+    # Test with OffsetArrays
+    if F === DiagonalPreconditioner
+        OA = OffsetMatrix(A, (-1, -1))
+        OD = OffsetVector(diag(A, 0), -1)
+        OP = DiagonalPreconditioner(OD)
+        b = ones(n)
+        ob = OffsetVector(b, -1)
+        err = ArgumentError("incompatible indices for input arguments")
+        @test_throws err OP \ b
+        @test_throws err ldiv!(OP, b)
+        @test_throws err ldiv!(b, OP, b)
+        @test OP \ ob ≈ ldiv!(OP, copy(ob)) ≈ ldiv!(similar(ob), OP, ob)
+        @test_throws err UpdatePreconditioner!(OP, A)
+        x = first(OP.D)
+        UpdatePreconditioner!(OP, OA + 2I)
+        @test first(OP.D) == x + 2
+    end
 end
 
 @testset "$T preconditioner" for (T, F) in (("Diagonal", DiagonalPreconditioner), ("Incomplete Cholesky", CholeskyPreconditioner), ("AMG Ruge-Stuben", RugeStuben), ("AMG Smoothed Aggregation", SmoothedAggregation))
