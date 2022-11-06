d842b807dcf84cc7488f903c8b353326ddd82f07
Jeff Fessler <fessler@umich.edu>
2022-11-06 15:03:32 -0500

    Allows kwargs to pass to lldl

--- src/incompletecholesky.jl
+++ src/incompletecholesky.jl
@@ -2,20 +2,30 @@ function assert_pd(d, α)
     @assert α == 0 && all(x -> x > 0, d) "The input matrix is not positive definite."
 end
 
+"""
+    CholeskyPreconditioner(A, memory=2; kwargs...)
+Construct
+`CholeskyPreconditioner` from matrix `A`
+for parameters `memory` and `kwargs` passed to
+`LimitedLDLFactorizations.lldl`.
+"""
 mutable struct CholeskyPreconditioner{L} <: AbstractPreconditioner
     ldlt::L
     memory::Int
-    function CholeskyPreconditioner(A, memory=2)
+    function CholeskyPreconditioner(A, memory=2; kwargs...)
         _A = get_data(A)
-        LLDL = lldl(_A, memory=memory)
+        LLDL = lldl(_A; memory, kwargs...)
         assert_pd(LLDL.D, LLDL.α)
         return new{typeof(LLDL)}(LLDL, memory)
     end
 end
 
-function UpdatePreconditioner!(C::CholeskyPreconditioner, A, memory=C.memory)
+"""
+    UpdatePreconditioner!(C::CholeskyPreconditioner, A, memory=C.memory; kwargs...)
+"""
+function UpdatePreconditioner!(C::CholeskyPreconditioner, A, memory=C.memory; kwargs...)
     _A = get_data(A)
-    LLDL = lldl(_A, memory=memory)
+    LLDL = lldl(_A; memory, kwargs...)
     assert_pd(LLDL.D, LLDL.α)
     C.ldlt = LLDL
     return C
