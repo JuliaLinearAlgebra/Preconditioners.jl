62e5d805d94129c934094682c7f339f43eb3415c
Jeff Fessler <fessler@umich.edu>
2022-11-06 14:47:47 -0500

    Initial docs

---
+++ docs/lit/examples/1-overview.jl
@@ -0,0 +1,86 @@
+#---------------------------------------------------------
+# # [Preconditioners overview](@id 1-overview)
+#---------------------------------------------------------
+
+#=
+This page illustrates some of the method(s) in the Julia package
+[`Preconditioners.jl`](https://github.com/JuliaLinearAlgebra/Preconditioners.jl).
+
+This page was generated from a single Julia file:
+[1-overview.jl](@__REPO_ROOT_URL__/1-overview.jl).
+=#
+
+#md # In any such Julia documentation,
+#md # you can access the source code
+#md # using the "Edit on GitHub" link in the top right.
+
+#md # The corresponding notebook can be viewed in
+#md # [nbviewer](http://nbviewer.jupyter.org/) here:
+#md # [`1-overview.ipynb`](@__NBVIEWER_ROOT_URL__/1-overview.ipynb),
+#md # and opened in [binder](https://mybinder.org/) here:
+#md # [`1-overview.ipynb`](@__BINDER_ROOT_URL__/1-overview.ipynb).
+
+
+# ### Setup
+
+# Packages needed here.
+
+using Preconditioners: DiagonalPreconditioner, CholeskyPreconditioner
+using InteractiveUtils: versioninfo
+using SparseArrays: sprand
+using LinearAlgebra: I, cond
+
+
+#=
+### Overview
+
+Preconditioning is useful
+for accelerating solutions
+to systems of equations
+and optimization problems.
+=#
+
+#=
+### Examples
+=#
+
+n = 1000
+A = sprand(n, n, 0.01)
+A = A + A' + 10I
+
+# Examine conditioning:
+cA = cond(Matrix(A), 2)
+
+# Diagonal preconditioner
+Dp = DiagonalPreconditioner(A)
+
+# apply preconditioner
+DA = Dp \ A
+
+# examine effect on condition number
+cd = cond(Matrix(DA), 2)
+
+# Incomplete Cholesky preconditioner with cut-off level 2
+Pc = CholeskyPreconditioner(A, 2)
+
+# Here is a temporary fix
+import Base.\
+\(C::CholeskyPreconditioner, A::AbstractMatrix) = reduce(hcat, [C \ c for c in eachcol(A)])
+
+# apply preconditioner
+CA = Pc \ A
+
+# examine effect on condition number
+cc = cond(Matrix(CA), 2)
+
+
+# ### Reproducibility
+
+# This page was generated with the following version of Julia:
+
+io = IOBuffer(); versioninfo(io); split(String(take!(io)), '\n')
+
+
+# And with the following package versions
+
+import Pkg; Pkg.status()
---
+++ docs/src/index.md
@@ -0,0 +1,15 @@
+```@meta
+CurrentModule = Preconditioners
+```
+
+# Preconditioners.jl Documentation
+
+## Overview
+
+The Julia package
+[Preconditioners.jl](https://github.com/JuliaLinearAlgebra/Preconditioners.jl)
+provides some
+[preconditioners](https://en.wikipedia.org/wiki/Preconditioner)
+for optimization problems.
+
+See the Examples tab for details.
